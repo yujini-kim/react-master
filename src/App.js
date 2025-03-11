514bfe2d62bb361abda65b511b65c38c95bcd172
yujin <kingyujin56@gmail.com>
2025-03-11 23:35:31 +0900

    feat: Add ThemeProvider
    - 이 부분에 변경 사항을 자세히 설명합니다.
    
    관련 이슈: #123

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes, ThemeConsumer } from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Wraaper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wraaper>
+      <Title>Hello</Title>
+    </Wraaper>
   );
 }
 
